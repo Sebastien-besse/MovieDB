6a65bb24404652fc30fdb19022f561d358a6d2c2
Sébastien <sebbody79400@gmail.com>
2024-12-31 16:14:38 +0100

    initial project

---
+++ MovieDB/MovieDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDBApp.swift
+//  MovieDB
+//
+//  Created by Sebastien Besse on 30/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct MovieDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

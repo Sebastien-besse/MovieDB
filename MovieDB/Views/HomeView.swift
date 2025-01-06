c7ebc6ed3e01e8f503ecb3544627eff8916f2b8d
Sébastien <sebbody79400@gmail.com>
2025-01-06 23:41:27 +0100

    Created Home View

---
+++ MovieDB/Views/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  MovieDB
+//
+//  Created by Sebastien Besse on 06/01/2025.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
